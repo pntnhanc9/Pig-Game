b64f4f512909cac934fcfb702ccbe680c546cdbf
Thien Nhan <thiennhanlop10b1@gmail.com>
2025-03-13 09:19:14 +0700

    Add rolling the dice functionality

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 'use strict';
-const score0El=document.querySelector('#score--0');
-const score1El=document.querySelector('#score--1');
-const diceEl=document.querySelector('.dice');
-
-score0El.textContent=0;
-score1El.textContent=0;
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+// Starting state of the game
+score0El.textContent = 0;
+score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // Generate random dice
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //   Check if dice=1
+  if(dice!==1)
+  {
+    // Add dice to current score
+    currentScore+=dice;
+    current0El.textContent=currentScore;
+  }
+});
